830ccf56d4c2fcc0e49afc380af1eb09821e0856
Sumit Srivastava <sumit@Sumits-MacBook-Pro.local>
2025-12-06 17:10:40 -0800

    PR-2 implementation

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,9 @@
 import books from '../data/books.json';
+import authors from '../data/authors.json';
 
 export const resolvers = {
   Query: {
     books: () => books,
+    authors: () => authors,
   },
 };
